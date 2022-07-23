20778835cf5518833736d6057beb4d6d5e24c289
IT19149936_Buddhi <it19149936@my.sliit.lk>
2022-07-23 14:26:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,35 +136,35 @@ def update_User():
 
 @app.route('/predict',methods=['GET'])
 def Predict():
-    request_json = request.get_json()
-    uid = request_json.get('id')
-    gender = request_json.get('gender')
-    weight = request_json.get('weight')
-    height = request_json.get('height')
-    age = request_json.get('age')
-    colLev = request_json.get('colLev')
-    heartRate = request_json.get('heartRate')
-    stroke = request_json.get('stroke')
-    entireLife100Cigarettes = request_json.get('entireLife100Cigarettes')
-    cigarettePerDay = request_json.get('cigarettePerDay')
+    # request_json = request.get_json()
+    # uid = request_json.get('id')
+    # gender = request_json.get('gender')
+    # weight = request_json.get('weight')
+    # height = request_json.get('height')
+    # age = request_json.get('age')
+    # colLev = request_json.get('colLev')
+    # heartRate = request_json.get('heartRate')
+    # stroke = request_json.get('stroke')
+    # entireLife100Cigarettes = request_json.get('entireLife100Cigarettes')
+    # cigarettePerDay = request_json.get('cigarettePerDay')
 
-    currentSmoker = 1
-    if(cigarettePerDay>=1):
-        currentSmoker = 1
-
-
-    BMI = weight/(height*height)
-
-    BPMeds=0
-    if (heartRate>=90):
-        BPMeds=1
+    # currentSmoker = 1
+    # if(cigarettePerDay>=1):
+    #     currentSmoker = 1
 
 
-    loaded_model = pickle.load(open('model/Heart.pickle', 'rb'))
-    result = loaded_model.predict([[gender, age, currentSmoker, cigarettePerDay, BPMeds, stroke, colLev, BMI, heartRate]])
+    # BMI = weight/(height*height)
+
+    # BPMeds=0
+    # if (heartRate>=90):
+    #     BPMeds=1
 
 
-    return result
+    # loaded_model = pickle.load(open('model/Heart.pickle', 'rb'))
+    # result = loaded_model.predict([[gender, age, currentSmoker, cigarettePerDay, BPMeds, stroke, colLev, BMI, heartRate]])
+
+
+    return "result"
 
 
 @app.route('/authUser',methods=['POST'])
