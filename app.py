fb80f07b2e9e9dcdd1907a4d58bd562d9dc79e23
IT19149936_Buddhi <it19149936@my.sliit.lk>
2022-09-10 13:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,88 +188,85 @@ def update_User():
         BroadJumpLength = request_json.get('BroadJumpLength')
 
         # make Heart Diabetics Prediction
-        # if ( str(request_json.get["gender"]) == "" or str(request_json.get["weight"]) == "" or 
-        # str(request_json.get["height"]) == "" or str(request_json.get["age"]) == "" or 
-        # str(request_json.get["colLev"]) == "" or str(request_json.get["heartRate"]) == "" or 
-        # str(request_json.get["DifWalk"]) == "" or str(request_json.get["sysBP"]) == "" or
-        #  str(request_json.get["stroke"]) == "" or str(request_json.get["currentSmoker"]) == "" or 
-        #  str(request_json.get["entireLife100Cigarettes"]) == "" or 
-        #  str(request_json.get["cigarettePerDay"]) == "" or str(request_json.get["BPMeds"]) == ""  ):
-        #     output = {
-        #                 'Heart' : "reqFill",
-        #                 'Diabetes': "reqFill",
-        #             }
-        # else:
-        #     Age = int(age)
-        #     F_Age = 0
-        #     if( 18<=Age<=24 ):
-        #         F_Age=1
-        #     elif( 25<=Age<=29 ):
-        #         F_Age=2
-        #     elif( 30<=Age<=34 ):
-        #         F_Age=3
-        #     elif( 35<=Age<=39 ):
-        #         F_Age=4
-        #     elif( 40<=Age<=44 ):
-        #         F_Age=5
-        #     elif( 45<=Age<=49 ):
-        #         F_Age=6
-        #     elif( 50<=Age<=54 ):
-        #         F_Age=7
-        #     elif( 55<=Age<=59 ):
-        #         F_Age=8
-        #     elif( 60<=Age<=64 ):
-        #         F_Age=9
-        #     elif( 65<=Age<=69 ):
-        #         F_Age=10
-        #     elif( 70<=Age<=74 ):
-        #         F_Age=11
-        #     elif( 75<=Age<=79 ):
-        #         F_Age=12
-        #     elif( Age>=80 ):
-        #         F_Age=13
+        if ( str(request_json.get["gender"]) == "" or str(request_json.get["weight"]) == "" or 
+        str(request_json.get["height"]) == "" or str(request_json.get["age"]) == "" or 
+        str(request_json.get["colLev"]) == "" or str(request_json.get["heartRate"]) == "" or 
+        str(request_json.get["DifWalk"]) == "" or str(request_json.get["sysBP"]) == "" or
+         str(request_json.get["stroke"]) == "" or str(request_json.get["currentSmoker"]) == "" or 
+         str(request_json.get["entireLife100Cigarettes"]) == "" or 
+         str(request_json.get["cigarettePerDay"]) == "" or str(request_json.get["BPMeds"]) == ""  ):
+            output = {
+                        'Msg' :"reqFill",
+                    }
+        else:
+            Age = int(age)
+            F_Age = 0
+            if( 18<=Age<=24 ):
+                F_Age=1
+            elif( 25<=Age<=29 ):
+                F_Age=2
+            elif( 30<=Age<=34 ):
+                F_Age=3
+            elif( 35<=Age<=39 ):
+                F_Age=4
+            elif( 40<=Age<=44 ):
+                F_Age=5
+            elif( 45<=Age<=49 ):
+                F_Age=6
+            elif( 50<=Age<=54 ):
+                F_Age=7
+            elif( 55<=Age<=59 ):
+                F_Age=8
+            elif( 60<=Age<=64 ):
+                F_Age=9
+            elif( 65<=Age<=69 ):
+                F_Age=10
+            elif( 70<=Age<=74 ):
+                F_Age=11
+            elif( 75<=Age<=79 ):
+                F_Age=12
+            elif( Age>=80 ):
+                F_Age=13
 
-        #     F_currentSmoker = 1
-        #     if(currentSmoker=="No"):
-        #         F_currentSmoker = 0
+            F_currentSmoker = 1
+            if(currentSmoker=="No"):
+                F_currentSmoker = 0
 
-        #     F_stroke=1
-        #     if(stroke=="No"):
-        #         F_stroke=0
+            F_stroke=1
+            if(stroke=="No"):
+                F_stroke=0
 
-        #     BMI = int(weight)/(int(height)*int(height))
+            BMI = int(weight)/(int(height)*int(height))
 
-        #     F_DifWalk=1
-        #     if(DifWalk=="No"):
-        #         F_DifWalk=0
+            F_DifWalk=1
+            if(DifWalk=="No"):
+                F_DifWalk=0
             
-        #     F_Gender=1
-        #     if(gender=="Female"):
-        #         F_Gender=0
+            F_Gender=1
+            if(gender=="Female"):
+                F_Gender=0
 
-        #     F_entireLife100Cigarettes=1
-        #     if(entireLife100Cigarettes=="No"):
-        #         F_entireLife100Cigarettes=0
+            F_entireLife100Cigarettes=1
+            if(entireLife100Cigarettes=="No"):
+                F_entireLife100Cigarettes=0
 
-        #     F_BPMeds=1
-        #     if(str(BPMeds)=="No"):
-        #         F_BPMeds=0
+            F_BPMeds=1
+            if(str(BPMeds)=="No"):
+                F_BPMeds=0
         
 
-        #     loaded_model_Heart = pickle.load(open('model/Heart.pickle', 'rb'))
-        #     reHeart = loaded_model_Heart.predict([[F_Gender, age, F_currentSmoker, int(cigarettePerDay), F_BPMeds, F_stroke, colLev, BMI, int(heartRate)]])
-        #     resultHeart = str(reHeart[0])
+            loaded_model_Heart = pickle.load(open('model/Heart.pickle', 'rb'))
+            reHeart = loaded_model_Heart.predict([[F_Gender, age, F_currentSmoker, int(cigarettePerDay), F_BPMeds, F_stroke, colLev, BMI, int(heartRate)]])
+            resultHeart = str(reHeart[0])
 
-        #     loaded_model_Diabetes = pickle.load(open('model/Diabetes.pickle', 'rb'))
-        #     reDiabetes = loaded_model_Diabetes.predict([[sysBP, int(colLev), BMI, F_entireLife100Cigarettes, F_DifWalk, F_Gender, F_Age]])
-        #     resultDiabetes = str(reDiabetes[0])
+            loaded_model_Diabetes = pickle.load(open('model/Diabetes.pickle', 'rb'))
+            reDiabetes = loaded_model_Diabetes.predict([[sysBP, int(colLev), BMI, F_entireLife100Cigarettes, F_DifWalk, F_Gender, F_Age]])
+            resultDiabetes = str(reDiabetes[0])
 
-        #     HeartRisk = resultHeart
-        #     DiabetesRisk= resultDiabetes
+            HeartRisk = resultHeart
+            DiabetesRisk= resultDiabetes
 
             #End of make prediction
-
-
 
         User.objects(id=uid).update(gender = gender, weight = weight, height = height, 
             age = age, colLev = colLev, heartRate = heartRate, stroke =stroke, currentSmoker = currentSmoker, 
