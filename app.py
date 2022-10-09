ed5ba808ab67f3cb6a65a080ba40de927da4d30b
Buddhi Chathuranga <53532224+Buddhi-Chathuranga@users.noreply.github.com>
2022-10-09 18:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,10 +46,25 @@ class User(db.Document):
     entireLife100Cigarettes = db.StringField()
     cigarettePerDay = db.StringField()
     BPMeds = db.StringField()
-    BP = db.StringField()
+    sysBP = db.StringField()
+    disBP = db.StringField()
     DifWalk = db.StringField()
     heartRisk = db.StringField()
     diabetesRisk = db.StringField()
+    humidity = db.StringField()
+    temp = db.StringField()
+    stepCount = db.StringField()
+
+    bodyDisoder = db.StringField()
+    insulinCount = db.StringField()
+    glucose = db.StringField()
+    alcoholicsStatus = db.StringField()
+    activityStatus = db.StringField()
+    bodyFat = db.StringField()
+    gripForce = db.StringField()
+    SitAndBendForwardLength = db.StringField()
+    SitUpsCount = db.StringField()
+    BroadJumpLength = db.StringField()
 
     def to_json(self):
         return  {
@@ -69,10 +84,24 @@ class User(db.Document):
             "entireLife100Cigarettes":self.entireLife100Cigarettes,
             "cigarettePerDay":self.cigarettePerDay,
             "BPMeds":self.BPMeds,
-            "BP":self.BP,
+            "sysBP":self.sysBP,
+            "disBP":self.disBP,
             "DifWalk":self.DifWalk,
             "heartRisk":self.heartRisk,
             "diabetesRisk":self.diabetesRisk,
+            "humidity": self.humidity,
+            "temp":self.temp,
+            "stepCount":self.stepCount,
+            "bodyDisoder":self.bodyDisoder,
+            "insulinCount":self.insulinCount,
+            "glucose":self.glucose,
+            "alcoholicsStatus":self.alcoholicsStatus,
+            "activityStatus":self.activityStatus,
+            "bodyFat":self.bodyFat,
+            "gripForce":self.gripForce,
+            "SitAndBendForwardLength":self.SitAndBendForwardLength,
+            "SitUpsCount":self.SitUpsCount,
+            "BroadJumpLength":self.BroadJumpLength,
         }
 
 
@@ -105,8 +134,10 @@ def addUserFunction():
         else: 
             user1 = User(name=name, phone=phone, email=email, password=password, gender = "", weight = "",
                         height = "", age = "", colLev = "", heartRate = "", stroke = "",currentSmoker="", 
-                        entireLife100Cigarettes = "", cigarettePerDay = "", BPMeds ="", BP ="", DifWalk="", 
-                        heartRisk="", diabetesRisk="")
+                        entireLife100Cigarettes = "", cigarettePerDay = "", BPMeds ="", sysBP ="", disBP="", DifWalk="", 
+                        heartRisk="", diabetesRisk="", humidity="", temp="", stepCount="", bodyDisoder="",insulinCount="",
+                        glucose="",activityStatus="", bodyFat="", gripForce="", SitAndBendForwardLength="",SitUpsCount="",
+                        BroadJumpLength="",)
             user1.save()
             data = {
                 'Message': "Success"
@@ -146,14 +177,111 @@ def update_User():
         entireLife100Cigarettes = request_json.get('entireLife100Cigarettes')
         cigarettePerDay = request_json.get('cigarettePerDay')
         BPMeds = request_json.get('BPMeds')
-        BP = request_json.get('BP')
+        sysBP = request_json.get('sysBP')
+        disBP = request_json.get('disBP')
         DifWalk = request_json.get('DifWalk')
 
+        bodyDisoder = request_json.get('bodyDisoder')
+        insulinCount = request_json.get('insulinCount')
+        glucose = request_json.get('glucose')
+        alcoholicsStatus = request_json.get('alcoholicsStatus')
+        activityStatus = request_json.get('activityStatus')
+        bodyFat = request_json.get('bodyFat')
+        gripForce = request_json.get('gripForce')
+        SitAndBendForwardLength = request_json.get('SitAndBendForwardLength')
+        SitUpsCount = request_json.get('SitUpsCount')
+        BroadJumpLength = request_json.get('BroadJumpLength')
+
+        # make Heart Diabetics Prediction
+        # if ( 
+        #     str(request_json.get["gender"]) == "" or str(request_json.get["weight"]) == "" or 
+        #     str(request_json.get["height"]) == "" or str(request_json.get["age"]) == "" or 
+        #     str(request_json.get["colLev"]) == "" or str(request_json.get["heartRate"]) == "" or 
+        #     str(request_json.get["DifWalk"]) == "" or str(request_json.get["sysBP"]) == "" or
+        #     str(request_json.get["stroke"]) == "" or str(request_json.get["currentSmoker"]) == "" or 
+        #     str(request_json.get["entireLife100Cigarettes"]) == "" or 
+        #     str(request_json.get["cigarettePerDay"]) == "" or str(request_json.get["BPMeds"]) == ""  
+        #     ):
+        #     output = {
+        #                 'Msg' :"reqFill",
+        #             }
+        # else:
+        Age = int(age)
+        F_Age = 0
+        if( 18<=Age<=24 ):
+            F_Age=1
+        elif( 25<=Age<=29 ):
+            F_Age=2
+        elif( 30<=Age<=34 ):
+            F_Age=3
+        elif( 35<=Age<=39 ):
+            F_Age=4
+        elif( 40<=Age<=44 ):
+            F_Age=5
+        elif( 45<=Age<=49 ):
+            F_Age=6
+        elif( 50<=Age<=54 ):
+            F_Age=7
+        elif( 55<=Age<=59 ):
+            F_Age=8
+        elif( 60<=Age<=64 ):
+            F_Age=9
+        elif( 65<=Age<=69 ):
+            F_Age=10
+        elif( 70<=Age<=74 ):
+            F_Age=11
+        elif( 75<=Age<=79 ):
+            F_Age=12
+        elif( Age>=80 ):
+            F_Age=13
+
+        F_currentSmoker = 1
+        if(currentSmoker=="No"):
+            F_currentSmoker = 0
+
+        F_stroke=1
+        if(stroke=="No"):
+            F_stroke=0
+
+        BMI = int(weight)/(int(height)*int(height))
+
+        F_DifWalk=1
+        if(DifWalk=="No"):
+            F_DifWalk=0
+            
+        F_Gender=1
+        if(gender=="Female"):
+            F_Gender=0
+
+        F_entireLife100Cigarettes=1
+        if(entireLife100Cigarettes=="No"):
+            F_entireLife100Cigarettes=0
+
+        F_BPMeds=1
+        if(str(BPMeds)=="No"):
+            F_BPMeds=0
+        
+
+        loaded_model_Heart = pickle.load(open('model/Heart.pickle', 'rb'))
+        reHeart = loaded_model_Heart.predict([[F_Gender, age, F_currentSmoker, int(cigarettePerDay), F_BPMeds, F_stroke, colLev, BMI, int(heartRate)]])
+        resultHeart = str(reHeart[0])
+
+        loaded_model_Diabetes = pickle.load(open('model/Diabetes.pickle', 'rb'))
+        reDiabetes = loaded_model_Diabetes.predict([[sysBP, int(colLev), BMI, F_entireLife100Cigarettes, F_DifWalk, F_Gender, F_Age]])
+        resultDiabetes = str(reDiabetes[0])
+
+        HeartRisk = resultHeart
+        DiabetesRisk= resultDiabetes
+
+            #End of make prediction
 
         User.objects(id=uid).update(gender = gender, weight = weight, height = height, 
             age = age, colLev = colLev, heartRate = heartRate, stroke =stroke, currentSmoker = currentSmoker, 
             entireLife100Cigarettes = entireLife100Cigarettes, cigarettePerDay = cigarettePerDay, BPMeds = BPMeds, 
-            BP = BP, DifWalk=DifWalk)
+            sysBP = sysBP, disBP=disBP, DifWalk=DifWalk, bodyDisoder =bodyDisoder , insulinCount =insulinCount , glucose = glucose,
+            alcoholicsStatus =alcoholicsStatus , activityStatus = activityStatus, bodyFat = bodyFat, gripForce = gripForce,
+            SitAndBendForwardLength = SitAndBendForwardLength,SitUpsCount = SitUpsCount, BroadJumpLength = BroadJumpLength,
+            heartRisk=HeartRisk, diabetesRisk=DiabetesRisk)
 
         output = {'Msg' : 'Success'}
         return output
@@ -180,13 +308,13 @@ def Predict_d():
         entireLife100Cigarettes = user_obj["entireLife100Cigarettes"]
         cigarettePerDay = user_obj["cigarettePerDay"]
         BPMeds = user_obj["BPMeds"]
-        BP = user_obj["BP"]
+        sysBP = user_obj["sysBP"]
         DifWalk = user_obj["DifWalk"]
         heartRisk = user_obj["heartRisk"]
         DiabetesRisk = user_obj["diabetesRisk"]
 
 
-        if ( str(user_obj["gender"]) == "" or str(user_obj["weight"]) == "" or str(user_obj["height"]) == "" or str(user_obj["age"]) == "" or str(user_obj["colLev"]) == "" or str(user_obj["heartRate"]) == "" or str(user_obj["DifWalk"]) == "" or str(user_obj["BP"]) == "" or
+        if ( str(user_obj["gender"]) == "" or str(user_obj["weight"]) == "" or str(user_obj["height"]) == "" or str(user_obj["age"]) == "" or str(user_obj["colLev"]) == "" or str(user_obj["heartRate"]) == "" or str(user_obj["DifWalk"]) == "" or str(user_obj["sysBP"]) == "" or
          str(user_obj["stroke"]) == "" or str(user_obj["currentSmoker"]) == "" or str(user_obj["entireLife100Cigarettes"]) == "" or str(user_obj["cigarettePerDay"]) == "" or str(user_obj["BPMeds"]) == ""  ):
             output = {
                         'Heart' : "reqFill",
@@ -257,15 +385,15 @@ def Predict_d():
             resultHeart = str(reHeart[0])
 
             loaded_model_Diabetes = pickle.load(open('model/Diabetes.pickle', 'rb'))
-            reDiabetes = loaded_model_Diabetes.predict([[BP, int(colLev), BMI, F_entireLife100Cigarettes, F_DifWalk, F_Gender, F_Age]])
+            reDiabetes = loaded_model_Diabetes.predict([[sysBP, int(colLev), BMI, F_entireLife100Cigarettes, F_DifWalk, F_Gender, F_Age]])
             # re = loaded_model.predict([[0, 61, 1, 30.0, 0.0, 0, 225.0, 28.58, 65.0]])
             resultDiabetes = str(reDiabetes[0])
 
-        output = {
-                    'Heart' : resultHeart,
-                    'Diabetes': resultDiabetes
-                }
-        return output 
+            output = {
+                        'Heart' : resultHeart,
+                        'Diabetes': resultDiabetes
+                    }
+            return output 
 
     except Exception as e:
         output = {
@@ -277,6 +405,8 @@ def Predict_d():
         return output
 
     # return output
+
+
 
 
 @app.route('/getRisks',methods=['GET','POST'])
@@ -409,7 +539,6 @@ def predictStress():
                     
                 }
         return output
-
 
 app.register_blueprint(MealPlan, url_prefix='/mealplan')
 app.register_blueprint(ExercisePlan,url_prefix='/exerciseplan')
