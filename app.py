91e8325b03382837c47ef7ff5199f9b512b2c99f
Buddhi Chathuranga <53532224+Buddhi-Chathuranga@users.noreply.github.com>
2022-10-10 15:03:46 +0530

    BMI = int(weight)/(((int(height))/(100))*(int(height)/(100)))

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def update_User():
         if(stroke=="No"):
             F_stroke=0
 
-        BMI = int(weight)/(((int(height))/(100))*int(height))
+        BMI = int(weight)/(((int(height))/(100))*(int(height)/(100)))
 
         F_DifWalk=1
         if(DifWalk=="No"):
@@ -281,7 +281,7 @@ def Predict_d():
             if(stroke=="No"):
                 F_stroke=0
 
-            BMI = int(weight)/(((int(height))/(100))*int(height))
+            BMI = int(weight)/(((int(height))/(100))*(int(height)/(100)))
 
             F_DifWalk=1
             if(DifWalk=="No"):
